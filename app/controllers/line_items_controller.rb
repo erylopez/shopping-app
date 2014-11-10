e8c39b8d619042a1ab96b10afde496c2a2067fef
Erika Lopez <lopez1erika@gmail.com>
2014-11-10 19:54:52 -0300

    [Carts] Display current cart and line items   - carts show view   - empty cart method and button   - line items gets combined

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,8 +31,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
@@ -68,14 +67,11 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
-  #...
 end
